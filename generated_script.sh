b5eb6660a3b8acb03bdc64419e223cbbc84915a8
AKhilesh <akhileshkrbhagat1@gmail.com>
2025-04-28 00:52:45 +0530

    Initial commit

--- generated_script.sh
+++ generated_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git init
+git remote add origin https://github.com/akhileshkrbhagat1/TeA-Terminal_Assistant.git
 git add .
 git commit -m "Initial commit"
-git remote add origin https://github.com/akhileshkrbhagat1/TeA-Terminal_Assistant.git
 git push -u origin main
